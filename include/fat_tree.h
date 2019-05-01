fa663be99af0d64cbcf1e2800a19b82b9d221477
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-01 12:33:25 -0300

    reorganazing

---
+++ include/fat_tree.h
@@ -0,0 +1,11 @@
+#include <util.h>
+
+T ft[N];
+
+int ft_vis[N];
+
+// Fat Tree Make
+void ft_make(int);
+
+// Fat Tree DFS
+void ft_dfs(int);
---
+++ src/fat_tree.c
@@ -0,0 +1,28 @@
+#include <fat_tree.h>
+
+// Fat Tree Make
+void ft_make(int) {
+  for (int i = 0; i < N; i++) {
+    ft[i] = -1;
+    ft_vis[i] = false;
+  }
+}
+
+// Fat Tree DFS
+void ft_dfs(int idx, int destiny) {
+  if (idx >= N) {
+    return;
+  }
+
+  if (idx == destiny) {
+    // do something
+  }
+  
+  int nxt = 2 * idx;
+  int connection = idx < 3 ? 2 : 1;
+  
+  for (int i = 1; i <= connection; i++) {
+    ft_dfs(nxt+1, destiny);
+    ft_dfs(nxt+2, destiny);
+  }
+}
