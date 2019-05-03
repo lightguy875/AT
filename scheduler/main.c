4b4f3c835d1f1effbb89718ba562ddc56a0083f3
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-03 11:05:54 -0300

    reorganazing

--- scheduler/main.c
+++ scheduler/main.c
@@ -6,16 +6,22 @@
 
 #include "job.h"
 
+// Data
+List *jobs;
+
+
+void on_receive_message (int seconds, char* filename) {
+  Job *job = job_create(seconds, filename);
+
+  list_push_back(jobs, job);
+}
+
 int main (int argc, char *argv[]) {
   int seconds = 0;
 
-  List *jobs = list_create();
+  jobs = list_create();
 
-  if (argc == 3 && try_cast_int(argv[1], &seconds) && argv[2] != NULL) {
-    Job *job = job_create(seconds, argv[2]);
-
-    list_push_back(jobs, job);
-  } 
+  // Do something
   
   list_destroy(jobs);
   
