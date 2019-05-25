d12bc76997de1837c19e8128d2dde83a3bf9e171
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-05-25 16:41:22 -0300

    Update and rename fat_tree.c to tree.c

--- server/tree.c
+++ server/tree.c
@@ -1,7 +1,7 @@
-#include "fat_tree.h"
+#include "tree.h"
 #include "manager.h"
 
-// Fat Tree Make
+// Tree Make
 void ft_make(int *ft, int *vis) {
   for (int i = 0; i < N; i++) {
     ft[i] = -1;
@@ -9,27 +9,24 @@ void ft_make(int *ft, int *vis) {
   }
 }
 
-// Fat Tree DFS
+// Tree DFS
 void ft_dfs(int *ft, int idx, int destiny) {
   if (idx >= N) {
     return;
   }
 
-	int pid;
+  int pid;
   if (idx == destiny){
-  //
+    // got on destiny
   }
   
   int nxt = 2 * idx;
-  // int connection = idx < 3 ? 2 : 1;
-  
-  //for (int i = 1; i <= connection; i++) {
+	
   ft_dfs(ft, nxt+1, destiny);
   ft_dfs(ft, nxt+2, destiny);
-  //}
 }
 
-// Fat Tree Up
+// Tree Up
 int ft_up (int idx) {
   return idx / 2;
 }
