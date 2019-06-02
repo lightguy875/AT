08fc8b988102ede9f2c4ee9d81c7dd374defd41f
claudiosegala <claudiosegalafilho@gmail.com>
2019-06-02 19:04:27 -0300

    Updating hypercube down

--- server/hypercube.c
+++ server/hypercube.c
@@ -1,19 +1,33 @@
 #include "hypercube.h"
 
 void hc_make(int *hc) {
-  for (int i = 0; i < N; i++) {
-    hc[i] = -1;
-  }
+	for (int i = 0; i < N; i++) {
+		hc[i] = -1;
+	}
 }
 
 int hc_up (int idx) {
-  return idx  - (idx&-idx);
+  	return idx  - (idx&-idx);
 }
 
+/*
+0000 -> 0001
+        0010 -> 0011
+        0100 -> 0101
+                0110
+        1000 -> 1001
+                1010 -> 1011
+                1100 -> 1101
+                        1110 -> 1111
+*/
 void hc_down(int idx, int ans[]) {
 	int n = sizeof(*ans) / sizeof(int);
 
-	for (int i = 0; i < n; i++) {
-		ans[i] = (i < 2) ? 2 * idx + i + 1 : -1;
+	for (int i = 0; i < n && i < 4; i++) {
+		if (idx & (1 << i)) {
+			ans[i] = idx | (1 << i);
+		} else {
+			break;
+		}
 	}
 }
